e081585a08b30e554e9163276502ea625455e483
xinyewen <xinyewen6@gmail.com>
2022-10-15 16:43:27 +0800

    加入前端项目（copy的）
    s

--- src/main/java/com/xsolo/XsoloApplication.java
+++ src/main/java/com/xsolo/XsoloApplication.java
@@ -2,12 +2,21 @@ package com.xsolo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class XsoloApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(XsoloApplication.class, args);
+    public static void main(String[] args) throws SQLException {
+        ConfigurableApplicationContext run = SpringApplication.run(XsoloApplication.class, args);
+        DataSource dataSource = run.getBean(DataSource.class);
+        System.out.println(dataSource);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
     }
 
 }
