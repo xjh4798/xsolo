e67c5d1f2fad165009aed40cef83c524eabe4412
xinyewen <xinyewen6@gmail.com>
2022-10-15 14:47:16 +0800

    初始化项目

---
+++ xsolo/src/main/java/com/xsolo/XsoloApplication.java
@@ -0,0 +1,13 @@
+package com.xsolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsoloApplication.class, args);
+    }
+
+}
